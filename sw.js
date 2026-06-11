14006cd24f52c975743aff37ea1440fca79d40c9
Kosuke510 <98221193+Kosuke510@users.noreply.github.com>
2026-06-11 15:35:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'seiya-v2';
+const CACHE = 'seiya-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
